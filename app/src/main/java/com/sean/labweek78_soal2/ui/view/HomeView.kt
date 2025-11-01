6f32b7c00a5c43911cd028e43e30d94cf4593aee
Sean Lawton <slawton01@student.ciputra.ac.id>
2025-11-01 14:35:15 +0700

    Push home

--- app/src/main/java/com/sean/labweek78_soal2/ui/view/HomeView.kt
+++ app/src/main/java/com/sean/labweek78_soal2/ui/view/HomeView.kt
@@ -1,4 +1,16 @@
 package com.sean.labweek78_soal2.ui.view
 
-class HomeView {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeView() {
+
+}
+
+
+@Composable
+@Preview(showBackground = true, showSystemUi = true)
+fun HomePreview(){
+
 }
